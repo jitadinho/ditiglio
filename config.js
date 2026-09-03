be603a3dc0b5f7d54e79ab82b25ca470102c47c5
jitadinho <samuel.coimbra078@gmail.com>
2026-09-03 13:36:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Preencha somente com a URL e a chave PUBLICÁVEL/ANON do Supabase.
 // Nunca coloque a chave service_role neste arquivo.
 window.DITIGLIO_CONFIG = {
-  supabaseUrl: 'https://SEU-PROJETO.supabase.co',
-  supabaseKey: 'SUA_CHAVE_PUBLICAVEL_OU_ANON'
+  supabaseUrl: 'https://ofunloyewwnlzoxswbpa.supabase.co',
+  supabaseKey: 'sb_publishable_RHOvmZ7Kdds1i2xL-QhNiA_8xah3y5H'
 };
