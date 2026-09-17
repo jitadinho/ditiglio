f939882df17b3c9beac58b9501dc2fec398d4dd4
jitadinho <samuel.coimbra078@gmail.com>
2026-09-17 18:04:01 -0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Preencha somente com a URL e a chave PUBLICÁVEL/ANON do Supabase.
-// Nunca coloque a chave service_role neste arquivo.
-window.DITIGLIO_CONFIG = {
-  supabaseUrl: 'https://ofunloyewwnlzoxswbpa.supabase.co',
-  supabaseKey: 'sb_publishable_RHOvmZ7Kdds1i2xL-QhNiA_8xah3y5H'
-};
